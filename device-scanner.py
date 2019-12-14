19cbc678b5ae839cee64681da9d205d6d3435d43
root <root@localhost.localdomain>
2019-12-14 23:40:58 +0800

    Updated device-scanner.py

--- device-scanner.py
+++ device-scanner.py
@@ -1,16 +1,13 @@
-import socket
-from threading import Thread
+import subprocess 
 
-ip = '192.168.1.'
+command = 'nmap -sP 192.168.1.0/24'
+data = subprocess.check_output(command.split()).split('\n')
 
-def main(i):
-    data = socket.gethostbyaddr(ip+str(i))
-    if data[0] != data[2][0]:
-        print("[+] %s \t\t %s" % (data[0], data[2][0]))
+# 2-dimensional lists contaning host:ip
+profiles = [a.split('for')[1][1:-1].split() for a in data if 'Nmap scan report' in a]
 
-for i in range(1, 256):
-    try:
-        t = Thread(target=main, args=(i, ))
-        t.start()
-    except:
-        pass
+# a list containing MAC Addresses
+#mac_address = [b.split()[2] for b in data if 'MAC Address' in b]
+
+for host, ip in profiles:
+    print('[+] Host: {0:<25} IP: {1}'.format(host, ip[1:]))
