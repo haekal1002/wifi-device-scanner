e2f9d71c4bbc15188b7a817db492d2d0f815c869
haekal1002 <muhammadhaekal333@gmail.com>
2019-12-15 17:50:55 +0700

    Add MAC Address look up

--- device-scanner.py
+++ device-scanner.py
@@ -1,17 +1,21 @@
 import subprocess
 import threading
+from getmac import get_mac_address
+
+ip_ad = []
 
 def net_scan(i):
     try:
-        ip = '192.168.1.'
-        command = 'ping -c 1 '+ip+str(i)
+        ip = '192.168.1.'+str(i)
+        command = 'ping -c 1 '+ip
         data = subprocess.check_output(command.split()).decode().split('\n')
         
         for line in data:
             #print(line)
             if '0% packet loss' in line: 
-                print('[+] {} is active'.format(ip+str(i)))
-    
+                #print('[+] {} is active'.format(ip+str(i)))
+                ip_ad.append(ip)
+ 
     except Exception as error:
         #print(error)
         #print('[-] Trouble with {}'.format(ip+str(i)))
@@ -23,4 +27,10 @@ for i in range(1, 255):
         t.start()
     except Exception as error:
         print('[-] {}'.format(error))
+
+
+for i in range(0, len(ip_ad)):
+    mac = get_mac_address(ip=ip_ad[i])
+    if mac != None:
+        print('[+] IP: {} \t MAC Address: {}'.format(ip_ad[i], mac))
 
